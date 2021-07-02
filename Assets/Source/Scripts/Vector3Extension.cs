3108374da2d93376ba00f402f095109a50fdf784
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-03 01:17:14 +0300

    Vectors Extensions

---
+++ Assets/Source/Scripts/Vector3Extension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Vector3Extension
+{
+    public static Vector3Int ToVector3Int(this Vector3 value)
+    {
+        return new Vector3Int((int)value.x, (int)value.y, (int)value.z);
+    }
+
+    public static Vector3Int RoundToInt(this Vector3 value)
+    {
+        return new Vector3Int(Mathf.RoundToInt(value.x), Mathf.RoundToInt(value.y), Mathf.RoundToInt(value.z));
+    }
+
+    public static Vector3 RoundToFloat(this Vector3 value)
+    {
+        return new Vector3(Mathf.Round(value.x), Mathf.Round(value.y), Mathf.Round(value.z));
+    }
+}
---
+++ Assets/Source/Scripts/Vector3IntExtension.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Vector3IntExtension
+{
+    
+}
