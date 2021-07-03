cb87ebcde60f745c23a593c713f50ecebe8d4f3c
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-03 03:07:55 +0300

    Custom Unity Events

---
+++ Assets/Source/Scripts/CustomUnityEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine.Events;
+
+
+namespace CustomUnityEvents
+{
+    [Serializable]
+    public class EventInt : UnityEvent<int> { }
+
+    [Serializable]
+    public class EventFloat : UnityEvent<float> { }
+}
