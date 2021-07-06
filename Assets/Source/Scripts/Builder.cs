5d55c8d81f7ed3f4b6b21a4e0a849f366243b98c
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-06 20:39:50 +0300

    Add new classes
    Builder - voxel builder
    Selector - voxel selector
    Editor - voxel editor

--- Assets/Source/Scripts/Builder.cs
+++ Assets/Source/Scripts/Builder.cs
@@ -2,51 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Builder : Mode
+public class Builder : MonoBehaviour
 {
-    public override void Tick()
+    // Start is called before the first frame update
+    void Start()
     {
-        SceneData.Extractor.SetActive(false);
-
-        if (!SceneData.ControlGUI.IsPanel)
-        {
-            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit hit;
-
-            CastResult castResult = VoxelRayCast.CastByMouse(SceneData.RayLength);
-            if (castResult != null)
-            {
-
-                if (SceneData.Chunk.InChunk(SceneData.Vector3FloatToInt(castResult.lastPoint)))
-                {
-                    SceneData.Extractor.SetActive(true);
-                    SceneData.Extractor.transform.position = castResult.lastPoint;
-
-                    if (SceneData.EventInput.MouseDown0)
-                    {
-                        int id = SceneData.ColorTest.id;
-                        SceneData.Chunk.CreateVoxel(SceneData.ColorTest.id, castResult.lastPoint);
-                    }
-                }
-            }
-        }
+        
     }
 
-    public override void Disable()
+    // Update is called once per frame
+    void Update()
     {
-        SceneData.Extractor.SetActive(false);
+        
     }
-
-    public override void Enable()
-    {
-
-    }
-
-
-    //public void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawLine(posRay, posRay + dirRay * l);
-    //}
-
 }
--- Assets/Source/Scripts/Editor.cs
+++ Assets/Source/Scripts/Editor.cs
@@ -2,55 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Editor : Mode
+public class Editor : MonoBehaviour
 {
-    private Vector3? _vertexPos;
-
-    public override void Tick()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (SceneData.EventInput.MouseDown0)
-        {
-            if (!SceneData.DragSystem.CheckCapture())
-            {
-                if (!SceneData.ControlGUI.IsPanel)
-                {
-                    CastResult castResult = VoxelRayCast.CastVerticesByMouse(SceneData.RayLength);
-
-                    if (castResult != null)
-                    {
-                        if (castResult.vertex != null)
-                        {
-                            Vector3 verPos = castResult.point + Vector3.one * 0.5f;
-                            Vector3 offsetPos = (Vector3)SceneData.Chunk.GetOffsetVertexByPos(verPos);
-                            _vertexPos = verPos;
-
-                            SceneData.DragSystem.SetPosition(verPos + offsetPos);
-                            SceneData.DragSystem.SetActive(true);
-                        }
-                    }
-                }
-            }
-        }
+        
     }
 
-    public void Tesss(Vector3 startPos, Vector3 offset)
+    // Update is called once per frame
+    void Update()
     {
-        if (_vertexPos != null && offset != Vector3.zero)
-        {
-            SceneData.Chunk.OffsetVertexByPos((Vector3)_vertexPos, offset);
-            SceneData.Chunk.UpdateMesh();
-            SceneData.DragSystem.OffsetPosition(offset);
-        }
-    }
-
-    public override void Disable()
-    {
-        SceneData.DragSystem.SetActive(false);
-        SceneData.DragSystem.drag -= Tesss;
-    }
-
-    public override void Enable()
-    {
-        SceneData.DragSystem.drag += Tesss;
+        
     }
 }
