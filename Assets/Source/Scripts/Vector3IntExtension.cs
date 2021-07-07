cf43876c74a0312b00c20727c5da1fdd660839dc
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-08 01:50:52 +0300

    Update Vector3IntExtension

--- Assets/Source/Scripts/Vector3IntExtension.cs
+++ Assets/Source/Scripts/Vector3IntExtension.cs
@@ -3,5 +3,13 @@ using System.Collections;
 
 public static class Vector3IntExtension
 {
-    
+    public static Vector3Int Forward(this Vector3Int value)
+    {
+        return new Vector3Int(0, 0, 1);
+    }
+
+    public static Vector3Int Back(this Vector3Int value)
+    {
+        return new Vector3Int(0, 0, -1);
+    }
 }
