5a2bed0617fc9ceef59cd4d9793f92cb10efddd6
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-10 01:46:30 +0300

    Update Vertex
    Add constructors

--- Assets/Source/Scripts/Vertex.cs
+++ Assets/Source/Scripts/Vertex.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Vertex
 {
-    public static Vertex()
+    public readonly Vector3 PivotPosition;
+    public Vector3 Position { get; private set; }
+
+    public Vertex()
     {
-        offset = 1;
+
     }
 
+    public Vertex(Vector3 position)
+    {
+        PivotPosition = Position = position;
+    }
 
-    public static int offset;
-    public Vector3 Position { get; private set; }
-    private Vector3 _offset;
+    public Vertex(Vertex vertex)
+    {
+        PivotPosition = vertex.PivotPosition;
+        Position = vertex.Position;
+    }
+
+    public Vertex(Vector3 pivotPosition, Vector3 offset)
+    {
+        PivotPosition = pivotPosition;
+        Position = PivotPosition + offset;
+    }
+
+    public void SetOffset(Vector3 offset)
+    {
+        Position = PivotPosition + offset;
+    }
+
+    public Vector3 GetOffset()
+    {
+        return Position - PivotPosition;
+    }
 }
