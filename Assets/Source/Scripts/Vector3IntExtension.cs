140e53b83dcfd8fc2e22875b9b9639215569e2da
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-10 01:47:58 +0300

    Update Vector3Extension

--- Assets/Source/Scripts/Vector3IntExtension.cs
+++ Assets/Source/Scripts/Vector3IntExtension.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public static class Vector3IntExtension
 {
+    public static Vector3 ToVector3(this Vector3Int value)
+    {
+        return new Vector3(value.x, value.y, value.z);
+    }
+
     public static Vector3Int Forward(this Vector3Int value)
     {
         return new Vector3Int(0, 0, 1);
