fefae40dbf093678d9a967d5b7cd48e471a43208
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-07-16 02:45:58 +0300

    Add Vector3Bool

---
+++ Assets/Source/Scripts/Vector3Bool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Bool
+{
+    public bool X, Y, Z;
+
+    public Vector3Bool()
+    {
+        X = Y = Z = false;
+    }
+
+    public Vector3Bool(bool x, bool y, bool z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Vector3Bool False => new Vector3Bool(false, false, false);
+
+    public static Vector3Bool True => new Vector3Bool(true, true, true);
+
+    public bool IsTrue => X || Y || Z;
+
+    public bool IsPureTrue => X && Y && Z;
+}
