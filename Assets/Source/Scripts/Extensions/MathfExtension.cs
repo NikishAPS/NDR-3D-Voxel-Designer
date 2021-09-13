f536e3121695d4284c396151c91e26b58a030cd2
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-09-13 19:58:05 +0300

    Add MathfExtension

---
+++ Assets/Source/Scripts/Extensions/MathfExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathfExtension
+{
+    public static float SinDeg(this Mathf mathf, float angle)
+    {
+        return Mathf.Sin(angle * Mathf.Deg2Rad);
+    }
+
+    public static float ConDeg(this Mathf mathf, float angle)
+    {
+        return Mathf.Cos(angle * Mathf.Deg2Rad);
+    }
+}
