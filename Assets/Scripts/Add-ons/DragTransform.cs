f7abfbd86871ca82f0ff0d98589955e8991e289c
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2021-09-28 01:20:00 +0300

    Update DragTransform

--- Assets/Scripts/Add-ons/DragTransform.cs
+++ Assets/Scripts/Add-ons/DragTransform.cs
@@ -4,6 +4,14 @@ public class DragTransform
 {
     public Vector3 Position { get; set; }
     public Vector3 Scale { get; set; }
+    public static DragTransform Zero => new DragTransform(Vector3.zero, Vector3.zero);
+
+
+    public DragTransform()
+    {
+        Position = Vector3.zero;
+        Scale = Vector3.zero;
+    }
 
     public DragTransform(Vector3 position, Vector3 scale)
     {
@@ -16,4 +24,10 @@ public class DragTransform
         Position = transform.position;
         Scale = transform.localScale;
     }
+
+    public static DragTransform operator +(DragTransform summand1, DragTransform summand2)
+    {
+        return new DragTransform(summand1.Position + summand2.Position, summand1.Scale + summand2.Scale);
+    }
+
 }
