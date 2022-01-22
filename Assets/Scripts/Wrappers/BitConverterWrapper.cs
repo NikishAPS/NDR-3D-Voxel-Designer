7306ef63322084a9b866bdd8dac0420474a8862f
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2022-01-23 01:24:20 +0300

    Update BitConverterWrapper

--- Assets/Scripts/Wrappers/BitConverterWrapper.cs
+++ Assets/Scripts/Wrappers/BitConverterWrapper.cs
@@ -3,6 +3,25 @@ using UnityEngine;
 
 public static class BitConverterWrapper
 {
+    public static byte[] GetBytes(Vector3 value)
+    {
+        byte[] result = new byte[24];
+
+        byte[] bytes = BitConverter.GetBytes(value.x);
+        for (int i = 0; i < bytes.Length; i++)
+            result[i] = bytes[i];
+
+        bytes = BitConverter.GetBytes(value.y);
+        for (int i = 0; i < bytes.Length; i++)
+            result[i + 8] = bytes[i];
+
+        bytes = BitConverter.GetBytes(value.z);
+        for (int i = 0; i < bytes.Length; i++)
+            result[i + 16] = bytes[i];
+
+        return result;
+    }
+
     public static byte[] GetBytes(Vector3Int value)
     {
         byte[] result = new byte[12];
