eb7163aea8125c5c53daeb6195698da51459fc6a
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2022-01-23 00:24:04 +0300

    Add ObjModelManager

---
+++ Assets/Scripts/Project/ObjModelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ObjModelManager
+{
+    public static ObjModel SelectedModel;
+
+    private static LinkedList<ObjModel> _models = new LinkedList<ObjModel>();
+
+    public static bool TryImport(string path)
+    {
+        if (!File.Exists(path)) return false;
+
+        GameObject model = new OBJImporter(path).Import();
+        _models.AddLast(new ObjModel(model, Voxelator.Center, 1));
+
+        return false;
+    }
+
+    public static void Select(ObjModel model)
+    {
+        SelectedModel = model;
+    }
+
+    public static bool Delete(ObjModel model)
+    {
+        if (!_models.Contains(model)) return false;
+        _models.Remove(model);
+        return true;
+    }
+
+}
