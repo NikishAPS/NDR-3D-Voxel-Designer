559d6e711aa3a8afd1d86a8734f4a4de645aae12
NikishAPS <50108069+NikishAPS@users.noreply.github.com>
2022-01-26 00:34:01 +0300

    Add binary stream
    Delete StreamManager
    Add Binary Stream Extension

---
+++ Assets/Scripts/Extensions/BinaryReaderExtension.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class BinaryReaderExtension
+{
+    public static Vector3Int ReadVector3Int(this BinaryReader binaryReader) =>
+        new Vector3Int(binaryReader.ReadInt32(), binaryReader.ReadInt32(), binaryReader.ReadInt32());
+
+    public static Vector3 ReadVector3(this BinaryReader binaryReader) =>
+        new Vector3(binaryReader.ReadSingle(), binaryReader.ReadSingle(), binaryReader.ReadSingle());
+
+    public static Vector3Byte ReadVector3Byte(this BinaryReader binaryReader) =>
+        new Vector3Byte(binaryReader.ReadByte(), binaryReader.ReadByte(), binaryReader.ReadByte());
+
+    public static bool TryRead(this BinaryReader binaryReader, out int result)
+    {
+        if (CanRead(binaryReader, sizeof(int)))
+        {
+            result = binaryReader.ReadInt32();
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    public static bool TryRead(this BinaryReader binaryReader, out float result)
+    {
+        if (CanRead(binaryReader, sizeof(float)))
+        {
+            result = binaryReader.ReadSingle();
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    public static bool TryRead(this BinaryReader binaryReader, out string result)
+    {
+        result = binaryReader.ReadString();
+        return result != null;
+    }
+
+    public static bool TryRead(this BinaryReader binaryReader, out Vector3Int result)
+    {
+        if (CanRead(binaryReader, sizeof(int) * 3))
+        {
+            result = binaryReader.ReadVector3Int();
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    public static bool TryRead(this BinaryReader binaryReader, out Vector3 result)
+    {
+        if (CanRead(binaryReader, sizeof(float) * 3))
+        {
+            result = ReadVector3(binaryReader);
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    public static bool TryRead(this BinaryReader binaryReader, out Vector3Byte result)
+    {
+        if(CanRead(binaryReader, sizeof(byte) * 3))
+        {
+            result = ReadVector3Byte(binaryReader);
+            return true;
+        }
+
+        result = default;
+        return false;
+    }
+
+    private static bool CanRead(BinaryReader binaryReader, int bytesCount)
+    {
+        return binaryReader.BaseStream.Length - binaryReader.BaseStream.Position >= bytesCount;
+    }
+
+}
---
+++ Assets/Scripts/Extensions/BinaryWriterExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+
+public static class BinaryWriterExtension
+{
+    public static void Write(this BinaryWriter binaryWriter, Vector3Int value)
+    {
+        binaryWriter.Write(value.x);
+        binaryWriter.Write(value.y);
+        binaryWriter.Write(value.z);
+    }
+
+    public static void Write(this BinaryWriter binaryWriter, Vector3 value)
+    {
+        binaryWriter.Write(value.x);
+        binaryWriter.Write(value.y);
+        binaryWriter.Write(value.z);
+    }
+}
